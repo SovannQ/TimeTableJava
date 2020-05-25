57f6b206974589fd86a1498a3df29d51de65599e
Etienne <etienne.rollin@edu.ece.fr>
2020-05-25 12:02:07 +0200

    Affichage prof

--- src/testdao/Professeur.java
+++ src/testdao/Professeur.java
@@ -10,5 +10,32 @@ package testdao;
  * @author titir
  */
 public class Professeur {
+    private int id = 0;
+    private int idCours = 0;
     
+    public Professeur(int id, int idCours) {
+    this.id = id;
+    this.idCours = idCours;
+  }
+    
+    public Professeur(){};
+    
+   public int getId()
+   {
+       return id;
+   }
+   public void setId(int id)
+   {
+       this.id = id;
+   }
+    
+   public int getIdCours()
+   {
+       return idCours;
+   }
+   public void setIdCours(int id)
+   {
+       this.id = id;
+   }
+   
 }
